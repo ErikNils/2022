d6c3e8b93700f745f79e7fe4e9cd067600543aed
Erik Nilsson <erik1.nilsson2@proton.me>
2023-08-25 16:10:46 +0200

    Day 7 Part 1 Complete

--- Day7/main.py
+++ Day7/main.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python3
+# #!/usr/bin/python3
 import os 
 import string
 from collections import deque
+from itertools import islice
 
 
 
@@ -37,7 +38,7 @@ def _cd(directory: str, stack: deque, sub_directories: dict[str, set]):
     elif directory == "..":
         stack.pop()
     elif directory in sub_directories[stack[-1]]:
-        stack.append(directory)
+        stack.append(f"/{directory}")
 
 
 def _ls(command: list[str], stack: deque[str], sub_directories: dict[str, set], directories: dict[str, dict[str, int]]):
@@ -47,15 +48,18 @@ def _ls(command: list[str], stack: deque[str], sub_directories: dict[str, set], 
         sub_dir = sub_directories[stack[-1]].add(command[1])
     
     elif command[0][0] in string.digits:
-        for directory in stack:
+        
+        for i in range(1,len(stack)+1):
+            directory_list = islice(stack,i)
+            directory = "".join(directory_list)
             if directory not in directories:
                 directories[directory] = {}
-            directories[directory][command[0]] = int(command[0])
+            directories[directory]["/".join(command[1])] = int(command[0])
             
 def _dir_size(directories: dict[str, dict[str, int]]):
     total_size = 0
     
-    del_dir = directories.get("/")
+
     
     for directory in directories:
         files = directories[directory]
@@ -64,8 +68,9 @@ def _dir_size(directories: dict[str, dict[str, int]]):
             dir_size += files[file]
         if dir_size <= 100000:
             total_size += dir_size
-            print(f"Directory: {directory}, Size: {dir_size}")
-        
+            # print(f"Directory: {directory}, Size: {dir_size}")
+    
+  
     
     return total_size
 
@@ -74,10 +79,51 @@ def main():
     file = [line.strip() for line in open(path, "r")]
     
     size = _execute(file)
-
     print(size)
 
 if __name__ == "__main__":
     main()
 
 
+# #!/usr/bin/python3
+# import sys
+# from collections import defaultdict
+# infile = sys.argv[1] if len(sys.argv)>1 else './Day7/input.csv'
+# data = open(infile).read().strip()
+# lines = [x for x in data.split('\n')]
+
+# # directory path -> total size of that directory (including subdirectories)
+# SZ = defaultdict(int)
+# path = []
+# for line in lines:
+#     words = line.strip().split()
+#     if words[1] == 'cd':
+#         if words[2] == '..':
+#             path.pop()
+#         else:
+#             path.append(words[2])
+#     elif words[1] == 'ls':
+#         continue
+#     elif words[0] == 'dir':
+#         continue
+#     else:
+#         sz = int(words[0])
+#         # Add this file's size to the current directory size *and* the size of all parents
+#         for i in range(1, len(path)+1):
+#             SZ['/'.join(path[:i])] += sz
+
+# max_used = 70000000 - 30000000
+# total_used = SZ['/']
+# need_to_free = total_used - max_used
+
+# p1 = 0
+# p2 = 1e9
+# for k,v in SZ.items():
+#     #print(k,v)
+#     if v <= 100000:
+#         p1 += v
+#         print(f"Directory: {k.split('/')[-1]}, Size: {v}")
+#     if v>=need_to_free:
+#         p2 = min(p2, v)
+# print(p1)
+# print(p2)
